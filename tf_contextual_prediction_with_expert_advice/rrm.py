21516a96d28c7f86f2cbe6f2fc9ce5d36dc33351
Dustin Morrill <dmorrill10@gmail.com>
2018-08-22 12:09:46 -0600

    Add `RrmLearner` and refactor loss methods.

--- tf_contextual_prediction_with_expert_advice/rrm.py
+++ tf_contextual_prediction_with_expert_advice/rrm.py
@@ -1,16 +1,19 @@
 import tensorflow as tf
 from tf_contextual_prediction_with_expert_advice import utility, rm_policy
+from tf_contextual_prediction_with_expert_advice.policy_model import \
+    PolicyModel, \
+    RmPolicyActivation
 
 
 def rrm_utilities(model, contexts, action_utilities):
     return utility(rm_policy(model(contexts)), action_utilities)
 
 
-def rrm_loss(regrets, action_utilities, ignore_negative_regrets=True):
-    regrets = tf.convert_to_tensor(regrets)
+def rrm_loss_given_policy(regrets,
+                          policy,
+                          action_utilities,
+                          ignore_negative_regrets=True):
     num_actions = regrets.shape[1].value
-    policy = rm_policy(regrets)
-
     inst_regret = tf.stop_gradient(
         action_utilities - tf.tile(
             utility(policy, action_utilities),
@@ -30,6 +33,15 @@ def rrm_loss(regrets, action_utilities, ignore_negative_regrets=True):
     return tf.reduce_mean(tf.reduce_sum(regret_diffs, axis=1)) / 2.0
 
 
+def rrm_loss(regrets, action_utilities, ignore_negative_regrets=True):
+    regrets = tf.convert_to_tensor(regrets)
+    return rrm_loss_given_policy(
+        regrets,
+        rm_policy(regrets),
+        action_utilities,
+        ignore_negative_regrets=ignore_negative_regrets)
+
+
 def rrm_grad(model, contexts, action_utilities, ignore_negative_regrets=True):
     with tf.GradientTape() as tape:
         loss_value = rrm_loss(
@@ -37,3 +49,24 @@ def rrm_grad(model, contexts, action_utilities, ignore_negative_regrets=True):
             action_utilities,
             ignore_negative_regrets=ignore_negative_regrets)
     return zip(tape.gradient(loss_value, model.variables), model.variables)
+
+
+class RrmLearner(RmPolicyActivation, PolicyModel):
+    def __init__(self, *args, ignore_negative_regrets=False, **kwargs):
+        super(RrmLearner, self).__init__(*args, **kwargs)
+        self.ignore_negative_regrets = ignore_negative_regrets
+
+    def loss(self, utility, inputs=None, predictions=None, policy=None):
+        if policy is None:
+            if predictions is None:
+                predictions = self.model(inputs)
+            return rrm_loss(
+                predictions,
+                utility,
+                ignore_negative_regrets=self.ignore_negative_regrets)
+        else:
+            return rrm_loss_given_policy(
+                predictions,
+                policy,
+                utility,
+                ignore_negative_regrets=self.ignore_negative_regrets)
