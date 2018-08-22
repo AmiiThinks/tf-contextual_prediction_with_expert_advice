7a951c2be4b701603de04af8e83d290ffe939b22
Dustin Morrill <dmorrill10@gmail.com>
2018-08-22 11:09:13 -0600

    Improve the class hierarchies in `policy_model`.

--- tf_contextual_prediction_with_expert_advice/policy_model.py
+++ tf_contextual_prediction_with_expert_advice/policy_model.py
@@ -3,7 +3,18 @@ from tf_contextual_prediction_with_expert_advice import norm_exp
 from tf_contextual_prediction_with_expert_advice.rrm import rm_policy
 
 
-class PolicyModel(object):
+class PolicyModelMixin(object):
+    def policy(self, inputs):
+        return self.policy_activation(self.model(inputs))
+
+    def policy_activation(self, pre_activations):
+        return pre_activations
+
+    def __call__(self, inputs):
+        return self.policy(inputs)
+
+
+class PolicyModel(PolicyModelMixin):
     @classmethod
     def load(cls, name):
         with open('{}.yml'.format(name), "r") as yaml_file:
@@ -20,15 +31,6 @@ class PolicyModel(object):
             yaml_file.write(self.model.to_yaml())
         self.model.save_weights('{}.h5'.format(name))
 
-    def policy(self, inputs):
-        return self.policy_activation(self.model(inputs))
-
-    def policy_activation(self, pre_activations):
-        return pre_activations
-
-    def __call__(self, inputs):
-        return self.policy(inputs)
-
 
 class NormExpPolicyActivation(object):
     def policy_activation(self, pre_activations):
@@ -40,25 +42,22 @@ class RmPolicyActivation(object):
         return rm_policy(pre_activations)
 
 
-class MetaPolicyModel(PolicyModel):
+class MetaPolicyModel(PolicyModelMixin):
     '''Uses a meta-policy to select between multiple policies generated
        from common model predictions.
     '''
 
-    def __init__(self, meta_policy, policy_activations, *args, **kwargs):
-        super(MetaPolicyModel, self).__init__(*args, **kwargs)
-        self.policy_activations = policy_activations
-        self.meta_policy = meta_policy
+    def __init__(self, model):
+        self.model = model
 
-    def num_policies(self):
-        return len(self.policy_activations)
+    def all_policy_activations(self, pre_activations):
+        raise RuntimeError('Unimplemented')
 
     def all_policies(self, inputs):
         return self._all_policies(self.model(inputs))
 
     def _all_policies(self, predictions):
-        return tf.stack(
-            [f(predictions) for f in self.policy_activations], axis=-1)
+        return tf.stack(self.all_policy_activations(predictions), axis=-1)
 
     def policy_activation(self, predictions):
         policies = self._all_policies(predictions)
